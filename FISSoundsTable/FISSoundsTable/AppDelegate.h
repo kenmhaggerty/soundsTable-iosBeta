f7af7361d131e2b4396c7953d1affbfcfdd2ecb6
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-05 13:00:28 -0500

    Recreates Sounds settings view controller.

---
+++ FISSoundsTable/FISSoundsTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FISSoundsTable
+//
+//  Created by Ken M. Haggerty on 2/5/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
